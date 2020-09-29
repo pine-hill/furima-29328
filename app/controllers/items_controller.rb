ba904d3e4170a4246dc0857d86086fbfbd2802cb
pine-hill <mat46.cutter.no9@gmail.com>
2020-09-29 10:46:32 +0900

    Revert "商品情報編集機能の実装"

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit ,:update]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -19,9 +20,18 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to action: :index
+    else
+      render :edit
+    end
+  end
   private
   def item_params
     params.require(:item).permit(
@@ -35,4 +45,7 @@ class ItemsController < ApplicationController
     end
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
